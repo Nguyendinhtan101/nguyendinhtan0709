8c63805810adfe38836669f1743202026ff41467
Your Name <you@example.com>
2022-09-13 21:15:44 +0700

    first commit

---
+++ Controllers/studentcontrollers.cs
@@ -0,0 +1,31 @@
+namespace nguyendinhtanbth.Controllers;
+
+
+public class Homecontroller : Controller
+{
+    private readonly ILogger<HomeController>_logger;
+
+     public HomeController (ILogger<HomeController>)logger
+     {
+        _logger=Logger ;
+
+     }
+
+
+
+
+
+
+
+//public class studentContrllers : Controllers
+
+
+    //cac action
+   // public int studentid
+
+
+
+
+
+
+
